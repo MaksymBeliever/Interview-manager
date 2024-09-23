d3f88df2e373c74e3005e2e2658ce1ea5ed0262a
MaksymBeliever <believer.maks@gmail.com>
2024-09-24 01:43:37 +0300

    setup project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import MainHeader from '@/components/MainHeader.vue'
+</script>
+
+<template>
+  <div class="container">
+    <main-header />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1280px;
+  margin: auto;
+  padding: 20px;
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <main-menubar :model="items" class="menu">
+    <template #item="{ item, props }">
+      <router-link v-bind="props.action" :to="item.path" class="flex align-items-center">
+        <span :class="item.icon" class="p-menuitem-icon"></span>
+        <span class="ml-2">{{ item.label }}</span>
+      </router-link>
+    </template>
+  </main-menubar>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface IMenuItem {
+  label: string,
+  icon: string,
+  path: string
+}
+
+const items = ref<IMenuItem[]>([
+  {
+    label: 'Authorisation',
+    icon: 'pi pi-user',
+    path: '/auth'
+  },
+  {
+    label: 'Add',
+    icon: 'pi pi-plus',
+    path: '/'
+  },
+  {
+    label: 'Interviews',
+    icon: 'pi pi-list',
+    path: '/interviews'
+  },
+  {
+    label: 'Statistics',
+    icon: 'pi pi-chart-pie',
+    path: '/statistics'
+  },
+]);
+
+</script>
+
+<style lang="scss" scoped>
+  .menu {
+    margin: 30px 0;
+    &-exit {
+      cursor: pointer;
+    }
+  }
+</style>
